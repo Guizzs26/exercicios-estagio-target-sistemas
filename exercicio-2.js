bb9ec23c8a2235f9c66da2abe76c567dbead2165
Guilherme <guiteixeira1926@gmail.com>
2024-03-06 08:06:17 -0400

    exercicio 2

---
+++ exercicio-2.js
@@ -0,0 +1,61 @@
+// Resolução com recursividade
+
+function fibonacci(num) {
+  function recursiveFibonacci(x) {
+    if (x <= 1) {
+      return x;
+    }
+
+    return recursiveFibonacci(x - 1) + recursiveFibonacci(x - 2);
+  }
+
+  let i = 0;
+
+  let fibonac = recursiveFibonacci(i);
+
+  while (fibonac <= num) {
+    if (fibonac === num) {
+      return true;
+    }
+    i++;
+
+    fibonac = recursiveFibonacci(i);
+  }
+  return false;
+}
+
+const testNumber = 34;
+
+console.log(
+  `${testNumber} ${
+    fibonacci(testNumber) ? "pertence" : "não pertence"
+  } a sequência de fibonacci.`
+);
+
+// Resolução sem recursividade
+
+function notRecursiveFibonacci(num) {
+  let previous = 0;
+  let current = 1;
+
+  while (current <= num) {
+    let isFibonacci = number === current ? true : false;
+
+    if (isFibonacci) {
+      return true;
+    }
+
+    let next = previous + current;
+    previous = current;
+    current = next;
+  }
+  return false;
+}
+
+const number = 21;
+
+console.log(
+  `${number} ${
+    fibonacci(number) ? "pertence " : "não pertence "
+  } a sequência de fibonacci`
+);
